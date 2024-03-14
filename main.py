834a0a7e8f2d9caf1e9b77709bfbf068e5ce3e00
rchrdmess <126493151+rchrdmess@users.noreply.github.com>
2024-03-14 22:22:19 +0000

    Initial Upload
    Main and requirements files.

---
+++ main.py
@@ -0,0 +1,10 @@
+from gb_website import create_app # Instruction to import the application function from the gb_website directory
+
+app = create_app() # Calls the app function
+
+
+if __name__ == '__main__':
+    """
+    Ensures app only runs directly and not when imported
+    """
+    app.run(debug=True) # False when deploy
