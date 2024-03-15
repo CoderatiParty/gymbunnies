3cae5ba29558d9d74d2da6b4bb8f2c54456b1a5b
Richard Messenger <rchrdmessenger@gmail.com>
2024-03-15 00:11:47 +0000

    Updating mian.py for deployment.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from gb_website import create_app # Instruction to import the application function from the gb_website directory
+from gb_website import create_app, db # Instruction to import the application function from the gb_website directory
 
 app = create_app() # Calls the app function
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     """
