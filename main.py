3583ec70ce186daa31ae5e48671092f59b414b99
Richard Messenger <rchrdmessenger@gmail.com>
2024-03-15 00:50:47 +0000

    Deploy = False

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from gb_website import create_app, db # Instruction to import the application fu
 
 app = create_app() # Calls the app function
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
     """
     Ensures app only runs directly and not when imported
     """
-    app.run(debug=True) # False when deploy
+    app.run(debug=False) # False when deploy
+ 
