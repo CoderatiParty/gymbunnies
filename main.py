84172764bfa81d6a1bddf17ee7d4ef010c97d704
Richard Messenger <rchrdmessenger@gmail.com>
2024-03-15 22:23:13 +0000

    Updating main.

--- main.py
+++ main.py
@@ -5,14 +5,3 @@ app = create_app() # Calls the app function
 
 with app.app_context():
     db.create_all()
-
-if __name__ == '__main__':
-    """
-    Ensures app only runs directly and not when imported
-    """
-    app.run(
-    host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=os.environ.get("DEBUG") # False when deploy
-    )
- 
